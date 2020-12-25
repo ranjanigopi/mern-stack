8f4242bf0bd5df921b198813155d104b2a5d6338
Ranjani Gopi <vranju95@gmail.com>
2020-12-24 18:04:31 -0600

    body-parser installation and usage

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 app.use(express.static('./static'));
+app.use(bodyParser.json());
 
-app.get('/hello', (req,res) => {
+app.get('/hello', (req, res) => {
     res.send('<p>HYYYY</p>');
-})
+});
 
-app.get('/apple', (req,res) => {
+app.get('/apple', (req, res) => {
     res.sendFile(__dirname + '/static/second.html');
-})
+});
 
 app.listen(8080);
